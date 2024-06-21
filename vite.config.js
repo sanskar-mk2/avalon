97c7013d6880ea8d7f87c0f00968eecc72952578
Sanskar Tiwari <sanskar.mk2@gmail.com>
2024-06-22 01:41:11 +0530

    savings

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), ViteImageOptimizer({
-        jpg: { quality: 65 },
+        jpg: { quality: 35 },
     }
     )],
 });
