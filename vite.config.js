ea298521be9b91ecb1ea9b752057a853591049db
Sanskar Tiwari <sanskar.mk2@gmail.com>
2024-06-28 00:39:24 +0530

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+// import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
